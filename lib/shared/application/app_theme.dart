1b543846201bf5da9cc0181595257c77cb7f7f1b
Abduqodir <blacktaller5@gmail.com>
2024-12-29 02:25:10 +0500

    features: added apptheme and app.dart realized added googlefonts and go route

--- lib/shared/application/app_theme.dart
+++ lib/shared/application/app_theme.dart
@@ -1,1 +1,64 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static ThemeData get light => ThemeData(
+        colorScheme: colorScheme,
+        textTheme: textTheme,
+        scaffoldBackgroundColor: Color(0xffFFFFFF),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xffFFFFFF),
+          centerTitle: true,
+          titleTextStyle: GoogleFonts.inter(
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+            color: Color(0xffFFFFFF),
+          ),
+          iconTheme: IconThemeData(
+            color: Color(0xff0D1634),
+            size: 24,
+          ),
+        ),
+      );
+
+  static ColorScheme get colorScheme => ColorScheme.light(
+        brightness: Brightness.light,
+        // seedColor: Colors.black,
+        primary: Color(0xff0064D2),
+        onPrimary: Color(0xffFFFFFF),
+        secondary: Color(0xff0D1634),
+        onSecondary: Color(0x25283180),
+        error: Color(0xffE01E1E),
+        onError: Color(0xffFCE9E9),
+        surface: Color(0xffFCE9E9),
+        onSurface: Color(0xff808991),
+      );
+
+  static TextTheme get textTheme => TextTheme(
+        headlineLarge: GoogleFonts.inter(
+          fontWeight: FontWeight.w700,
+          fontSize: 34,
+          color: Color(0x0fffffff),
+        ),
+        headlineSmall: GoogleFonts.inter(
+          fontWeight: FontWeight.w500,
+          fontSize: 24,
+          color: Color(0xff0D1634),
+        ),
+        titleLarge: GoogleFonts.inter(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+          color: Color(0x0fffffff),
+        ),
+        titleSmall: GoogleFonts.inter(
+          fontWeight: FontWeight.w500,
+          fontSize: 12,
+          color: Color(0x25283180),
+        ),
+        bodyLarge: GoogleFonts.inter(
+          fontWeight: FontWeight.w500,
+          fontSize: 16,
+          color: Color(0xff0D1634),
+        ),
+      );
+}
--- lib/shared/presentation/app.dart
+++ lib/shared/presentation/app.dart
@@ -5,6 +5,9 @@ class FlightApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Placeholder(),
+    );
   }
 }
